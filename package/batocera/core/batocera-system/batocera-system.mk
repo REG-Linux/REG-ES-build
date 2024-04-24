2a41d8b9423bdd0da915c746a363cbd904d1071f
Romain TISSERAND <romain.tisserand@gmail.com>
2024-04-24 20:55:23 +0200

    Prune/speedup, next is external toolchain

--- package/batocera/core/batocera-system/batocera-system.mk
+++ package/batocera/core/batocera-system/batocera-system.mk
@@ -9,7 +9,7 @@ BATOCERA_SYSTEM_SOURCE=
 BATOCERA_SYSTEM_VERSION = 24.04-dev
 BATOCERA_SYSTEM_DATE_TIME = $(shell date "+%Y/%m/%d %H:%M")
 BATOCERA_SYSTEM_DATE = $(shell date "+%Y/%m/%d")
-BATOCERA_SYSTEM_DEPENDENCIES = tzdata
+BATOCERA_SYSTEM_DEPENDENCIES = 
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2837),y)
 	BATOCERA_SYSTEM_ARCH=bcm2837
@@ -81,33 +81,6 @@ endif
 
 define BATOCERA_SYSTEM_INSTALL_TARGET_CMDS
 
-	# version/arch
-	mkdir -p $(TARGET_DIR)/usr/share/batocera
-	echo -n "$(BATOCERA_SYSTEM_ARCH)" > $(TARGET_DIR)/usr/share/batocera/batocera.arch
-	echo $(BATOCERA_SYSTEM_VERSION)$(BATOCERA_SYSTEM_COMMIT) $(BATOCERA_SYSTEM_DATE_TIME) > $(TARGET_DIR)/usr/share/batocera/batocera.version
-
-	# datainit
-	mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/system
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-system/batocera.conf $(TARGET_DIR)/usr/share/batocera/datainit/system
-
-	# batocera-boot.conf
-	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-system/batocera-boot.conf $(BINARIES_DIR)/batocera-boot.conf
-
-	# sysconfigs (default batocera.conf for boards)
-	mkdir -p $(TARGET_DIR)/usr/share/batocera/sysconfigs
-        if test -d $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-system/sysconfigs/${BATOCERA_SYSTEM_ARCH}; then cp -pr $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-system/sysconfigs/${BATOCERA_SYSTEM_ARCH}/* $(TARGET_DIR)/usr/share/batocera/sysconfigs; fi
-
-	# mounts
-	mkdir -p $(TARGET_DIR)/boot $(TARGET_DIR)/overlay $(TARGET_DIR)/userdata
-
-	# variables
-	mkdir -p $(TARGET_DIR)/etc/profile.d
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-system/xdg.sh $(TARGET_DIR)/etc/profile.d/xdg.sh
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-system/dbus.sh $(TARGET_DIR)/etc/profile.d/dbus.sh
-
-	# list of modules that doesnt like suspend
-	mkdir -p $(TARGET_DIR)/etc/pm/config.d
-	echo 'SUSPEND_MODULES="rtw88_8822ce snd_pci_acp5x"' > $(TARGET_DIR)/etc/pm/config.d/config
 endef
 
 $(eval $(generic-package))
